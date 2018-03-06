11e5a2c9199bf2f73a56ae8f54ad7395d6b2f4c5
Emily Zhao <tong.zhao.92@gmail.com>
2018-03-06 16:11:18 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,4 @@
-INSERT INTO bears (name) VALUES ('Mr. Chocolate', 'Rowdy', 'Tabitha','Sergeant Brown', 'Melissa', 'Grinch', 'Wendy', NULL)
+INSERT INTO bears (name) VALUES ('Mr. Chocolate'); 
+INSERT INTO bears (name) VALUES ('Rowdy');
+INSERT INTO bears (name) VALUES ('Tabitha');
+INSERT INTO bears (name) VALUES ('Sergeant Brown'), 'Melissa', 'Grinch', 'Wendy', NULL)
